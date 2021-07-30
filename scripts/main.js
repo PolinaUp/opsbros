3a3576bec502d8ee4019a8b7d2e2204d1ce03ebd
olexoi <opoplavskiy64@gmail.com>
2021-07-30 16:54:22 +0300

    mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,46 @@
 $('.contact-btn-content').on('click', function () {
     $(this).toggleClass('active-contact-btn');
-
-    $('.calendly-badge-widget').css({
-        'opacity': '0',
-        'transform': 'rotate(-360deg)',
-        'bottom': '30px',
-        'transition-duration': '1.3s',
-    });
-
-    // css({
-    //     'background': 'url(/media/icons/cross.svg)',
-    //     'background-size': '45px 45px',
-    //     'background-position': '28px 28px',
-    //     'background-repeat': 'no-repeat',
-    //     'transform': 'rotate(360deg)',
-    //     'transition-duration': '1.3s',
-
-    // });
-    if ($(this).hasClass('active-contact-btn')) {
+    if($(window).width() < 1200) {
         $('.calendly-badge-widget').css({
-            'opacity': '1',
-            'transform': 'rotate(360deg)',
-            'bottom': '130px',
+            'opacity': '0',
+            'transform': 'rotate(-360deg)',
+            'bottom': '30px',
             'transition-duration': '1.3s',
         });
+    
+        if ($(this).hasClass('active-contact-btn')) {
+            $('.calendly-badge-widget').css({
+                'opacity': '1',
+                'transform': 'rotate(360deg)',
+                'bottom': '100px',
+                'transition-duration': '1.3s',
+                'z-index': '3452535453634',
+            });
+        }
+    } else {
+        $('.calendly-badge-widget').css({
+            'opacity': '0',
+            'transform': 'rotate(-360deg)',
+            'bottom': '30px',
+            'transition-duration': '1.3s',
+        });
+    
+        if ($(this).hasClass('active-contact-btn')) {
+            $('.calendly-badge-widget').css({
+                'opacity': '1',
+                'transform': 'rotate(360deg)',
+                'bottom': '130px',
+                'transition-duration': '1.3s',
+            });
+        }
     }
+   
+});
+$('.open-menu-mobile').on('click', function () {
+    $('#check').toggleClass('closed').toggleClass('opened');
+    $('.mobile-menu').toggleClass('display-none');
+});
+$('.menu-item').on('click', function () {
+    $('.mobile-menu').addClass('display-none');
+    $('#check').toggleClass('closed').toggleClass('opened');
 });
