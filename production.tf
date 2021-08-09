4d8f21c4fa5d84d3af153b307a0170b9af932c9a
olexoi <opoplavskiy64@gmail.com>
2021-08-09 15:01:12 +0300

    deployment fixes

--- production.tf
+++ production.tf
@@ -1,11 +1,14 @@
+
 provider "aws" {
   region = "eu-central-1"
+
 }
 
 resource "aws_s3_bucket" "opsbros_s3" {
   acl    = "public-read"
   bucket = "s3-opsbros-text.hashicorp.com"
   policy = file("policy.json")
+
 
   website {
     index_document = "index.html"
@@ -14,9 +17,75 @@ resource "aws_s3_bucket" "opsbros_s3" {
 }
 
 
-resource "aws_s3_bucket_object" "object" {
-  for_each = fileset(path.module, "deployment/**/*")
-  bucket   = "s3-opsbros-text.hashicorp.com"
-  key      = replace(each.value, "deployment", "")
+resource "aws_s3_bucket_object" "object_html" {
+
+  content_type = "text/html"
+
+  # for_each = fileset(path.module, "deployment/**/*")
+  bucket = "s3-opsbros-text.hashicorp.com"
+  key    = replace(each.value, "deployment", "")
+  # source   = each.value
+  for_each = fileset(path.module, "deployment/*")
   source   = each.value
+  # key      = each.value
+
+
+}
+resource "aws_s3_bucket_object" "object_css" {
+
+  content_type = "text/css"
+
+  # for_each = fileset(path.module, "deployment/**/*")
+  bucket = "s3-opsbros-text.hashicorp.com"
+  key    = replace(each.value, "deployment", "")
+  # source   = each.value
+  for_each = fileset(path.module, "deployment/style/*")
+  source   = each.value
+  # key      = each.value
+
+
+}
+
+resource "aws_s3_bucket_object" "object_svg" {
+
+  content_type = "image/svg+xml"
+
+  # for_each = fileset(path.module, "deployment/**/*")
+  bucket = "s3-opsbros-text.hashicorp.com"
+  key    = replace(each.value, "deployment", "")
+  # source   = each.value
+  for_each = fileset(path.module, "deployment/media/**/*")
+  source   = each.value
+  # key      = each.value
+
+
+}
+resource "aws_s3_bucket_object" "object_ttf" {
+
+  content_type = "font/ttf"
+
+  # for_each = fileset(path.module, "deployment/**/*")
+  bucket = "s3-opsbros-text.hashicorp.com"
+  key    = replace(each.value, "deployment", "")
+  # source   = each.value
+  for_each = fileset(path.module, "deployment/fonts/*")
+  source   = each.value
+  # key      = each.value
+
+
+}
+
+resource "aws_s3_bucket_object" "object_js" {
+
+  content_type = "text/javascript"
+
+  # for_each = fileset(path.module, "deployment/**/*")
+  bucket = "s3-opsbros-text.hashicorp.com"
+  key    = replace(each.value, "deployment", "")
+  # source   = each.value
+  for_each = fileset(path.module, "deployment/scripts/*")
+  source   = each.value
+  # key      = each.value
+
+
 }
