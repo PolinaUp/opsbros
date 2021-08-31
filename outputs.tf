76e2098938b68b85fd389a8cd30f78a0b2080248
olexoi <opoplavskiy64@gmail.com>
2021-08-31 19:54:57 +0300

    color changes

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+
+output "bucket" {
+  value = aws_s3_bucket.opsbros_s3.id
+}
--- production.tf
+++ production.tf
@@ -1,90 +1,59 @@
+terraform {
+  backend "s3" {
+    bucket = "opsbros.company"
+    key    = "state"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
-  region = "eu-central-1"
-
+  region                  = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "default"
 }
+
+# resource "aws_route53_record" "www" {
+#   zone_id = "Z06739541CIDNKNREB3JT"
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = "300"
+#   records = [aws_eip.lb.public_ip]
+#
+# }
 
 resource "aws_s3_bucket" "opsbros_s3" {
   acl    = "public-read"
-  bucket = "s3-opsbros-text.hashicorp.com"
+  bucket = "opsbros.company"
   policy = file("policy.json")
+
 
   website {
     index_document = "index.html"
+    # website_domain = "s3-opsbros.s3-website-us-east-1.amazonaws.com"
   }
 
 }
 
-
-resource "aws_s3_bucket_object" "object_html" {
-
-  content_type = "text/html"
-
-  # for_each = fileset(path.module, "deployment/**/*")
-  bucket = "s3-opsbros-text.hashicorp.com"
-  key    = replace(each.value, "deployment", "")
-  # source   = each.value
-  for_each = fileset(path.module, "deployment/*")
-  source   = each.value
-  # key      = each.value
-
-
+resource "null_resource" "remove_and_upload_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/deployment s3://${aws_s3_bucket.opsbros_s3.id}"
+  }
 }
-resource "aws_s3_bucket_object" "object_css" {
 
-  content_type = "text/css"
-
-  # for_each = fileset(path.module, "deployment/**/*")
-  bucket = "s3-opsbros-text.hashicorp.com"
-  key    = replace(each.value, "deployment", "")
-  # source   = each.value
-  for_each = fileset(path.module, "deployment/style/*")
-  source   = each.value
-  # key      = each.value
-
+resource "aws_instance" "import" {
+  instance_type          = "t2.micro"
+  ami                    = "ami-0c2b8ca1dad447f8a"
+  key_name               = "ec2"
+  vpc_security_group_ids = [aws_security_group.import_security_group.id]
+  tags = {
+    env  = "staging"
+    Name = "imported"
+  }
 
 }
 
-resource "aws_s3_bucket_object" "object_svg" {
-
-  content_type = "image/svg+xml"
-
-  # for_each = fileset(path.module, "deployment/**/*")
-  bucket = "s3-opsbros-text.hashicorp.com"
-  key    = replace(each.value, "deployment", "")
-  # source   = each.value
-  for_each = fileset(path.module, "deployment/media/**/*")
-  source   = each.value
-  # key      = each.value
-
-
-}
-resource "aws_s3_bucket_object" "object_ttf" {
-
-  content_type = "font/ttf"
-
-  # for_each = fileset(path.module, "deployment/**/*")
-  bucket = "s3-opsbros-text.hashicorp.com"
-  key    = replace(each.value, "deployment", "")
-  # source   = each.value
-  for_each = fileset(path.module, "deployment/fonts/*")
-  source   = each.value
-  # key      = each.value
-
-
-}
-
-resource "aws_s3_bucket_object" "object_js" {
-
-  content_type = "text/javascript"
-
-  # for_each = fileset(path.module, "deployment/**/*")
-  bucket = "s3-opsbros-text.hashicorp.com"
-  key    = replace(each.value, "deployment", "")
-  # source   = each.value
-  for_each = fileset(path.module, "deployment/scripts/*")
-  source   = each.value
-  # key      = each.value
+resource "aws_security_group" "import_security_group" {
+  name = "import_security_group"
 
 
 }
